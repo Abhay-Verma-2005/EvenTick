95c79f65461356c213aea60141e45a14914921ea
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2026-07-28 21:55:42 +0530

    Delete backend directory

--- backend/routes/statsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const statsController = require('../controllers/statsController');
-const { authMiddleware } = require('../middlewares/auth');
-
-router.get('/provider', authMiddleware, statsController.getProviderStats);
-router.get('/organiser', authMiddleware, statsController.getOrganiserStats);
-
-module.exports = router;
